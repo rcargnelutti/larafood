5e8e4e233f856e2355de50cf3a6d8d2fd926e8c0
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-06-08 02:31:57 -0300

    Módulo 09 - Aula 02

---
+++ database/seeders/TenantsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\{
+    Tenant,
+    Plan
+};
+use Illuminate\Database\Seeder;
+
+class TenantsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $plan = Plan::first();
+
+        $plan->tenants()->create([
+            'cnpj' => '23882706000120',
+            'name' => 'EspecializaTi',
+            'url' => 'especializati',
+            'email' => 'carlos@especializati.com.br',
+        ]);
+    }
+}
