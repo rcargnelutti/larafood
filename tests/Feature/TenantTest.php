f8118f40a96184bf6cf4429b31a5fde9d16ab924
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-07-12 02:39:16 -0300

    Modulo 22 - Aula 04

--- tests/Feature/TenantTest.php
+++ tests/Feature/TenantTest.php
@@ -16,13 +16,43 @@ class TenantTest extends TestCase
      */
     public function testGetAllTenants()
     {
+        //Tenant::truncate();
         //https://laravel.com/docs/8.x/database-testing
         Tenant::factory()->count(10)->create();
         //factory(Tenant::class, 10)->create();
 
         $response = $this->getJson('/api/v1/tenants');
-
+        //$response->dump();
         $response->assertStatus(200)
                     ->assertJsonCount(10, 'data');
+    }
+
+    /**
+     * Test Get Error Single Tenants
+     *
+     * @return void
+     */
+    public function testErrorGetTenant()
+    {
+        //Tenant::factory()->create();
+        $tenant = 'fake_value';
+
+        $response = $this->getJson("/api/v1/tenants/{$tenant}");
+        //$response->dump();
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Test Get Error Single Tenants
+     *
+     * @return void
+     */
+    public function testGetTenantByIdentify()
+    {
+        $tenant = Tenant::factory()->create();
+
+        $response = $this->getJson("/api/v1/tenants/{$tenant->uuid}");
+        //$response->dump();
+        $response->assertStatus(200);
     }
 }
