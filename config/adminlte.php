4f58b420cd492ebab8d39cf1d0658b03f038e666
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-07-14 00:53:42 -0300

    Modulo 23 - Aula 07

--- config/adminlte.php
+++ config/adminlte.php
@@ -46,7 +46,8 @@ return [
     */
 
     'logo' => '<b>LaraFood</b>',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    //'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => 'imgs/favicon.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
--- resources/views/site/home-index.blade.php
+++ resources/views/site/home-index.blade.php
@@ -1,4 +1,4 @@
-@extends('site.app');
+@extends('site.app')
 
 @section('content')
 
